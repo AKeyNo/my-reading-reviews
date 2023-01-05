7117f260260fe0d68bc7c899e88030103dec4b95
Ariel Aquino <arielaquino.cs@gmail.com>
2023-01-05 14:19:46 -0800

    Made background consistent rgb(17, 24, 39) a.k.a. bg-gray-900

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ export const Layout: React.FC<PropsWithChildren<any>> = ({ children }) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <div className='flex-1 bg-gray-900 mx-72'>{children}</div>
+      <div className='flex-1 mx-72'>{children}</div>
       <Footer />
     </div>
   );
