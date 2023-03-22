10132f7d6016f3f14636ad7a10c28befe10c2705
Ariel Aquino <arielaquino.cs@gmail.com>
2023-03-21 19:07:20 -0700

    Adjust button and header resizing

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -4,12 +4,12 @@ export const AuthButtons = () => {
   const router = useRouter();
 
   return (
-    <div className='-mb-20'>
+    <div className='flex -mb-20 space-x-4'>
       <button
         onClick={() => {
           router.push('/signup');
         }}
-        className='px-12 py-4 mx-32 duration-200 bg-orange-700 rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-orange-800 hover:drop-shadow-lg'
+        className='flex-auto w-12 px-2 py-4 duration-200 bg-orange-700 rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-orange-800 hover:drop-shadow-lg'
         data-cy='sign-up-button'
       >
         Sign Up
@@ -18,7 +18,7 @@ export const AuthButtons = () => {
         onClick={() => {
           router.push('/signin');
         }}
-        className='px-12 py-4 mx-32 duration-200 bg-orange-700 rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-orange-800 hover:drop-shadow-lg'
+        className='flex-auto w-12 px-2 py-4 duration-200 bg-orange-700 rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-orange-800 hover:drop-shadow-lg'
         data-cy='sign-in-button'
       >
         Sign In
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home({ session }: Props) {
         <title>My Reading Reviews - Keep track of your readings easily.</title>
       </Head>
       {!session && (
-        <div className='p-12 m-12 text-center bg-gray-800 border-4 border-gray-700 rounded-md'>
+        <div className='p-12 mb-12 text-center bg-gray-800 border-4 border-gray-700 rounded-md'>
           <h2 className='text-4xl font-bold'>Keep track of your readings.</h2>
           <h3 className='py-8 text-xl font-semibold text-blue-300'>
             Easy, quick, and made for readers.
